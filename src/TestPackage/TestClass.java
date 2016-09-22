974ed6e7ca1a839d0449db1cfa2d2ebb9d05ed54
ErichLeister <jasonvorhe@interia.pl>
2016-09-22 21:19:19 +0200

    dodalem dodawanie jas

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass
     System.out.println("test git");
     System.out.println("komentarz od jana i drugi komentarz i trzeci");
     
+    System.out.println(2+2);
     //przykladowa zmiana - test pull-a
     //druga zmiana wprowadzona przez github
     //test push to upstream
