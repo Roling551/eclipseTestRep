796f0c75fc4e5415ccfd5c6c02213b23a050a7e8
ErichLeister <jasonvorhe@interia.pl>
2016-09-22 19:40:39 +0200

    komentarz od jana

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass
 {
   public static void main(String[] args) {
     System.out.println("test git");
+    System.out.println("komentarz od jana");
     //przykladowa zmiana - test pull-a
     //druga zmiana wprowadzona przez github
   }
