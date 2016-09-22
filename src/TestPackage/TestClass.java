0294a743ace17914fa98fcd7d7677e8b1675c04e
Roling551 <Roling95@gmail.com>
2016-09-22 05:19:07 +0200

    druga zmiana wprowadzona przez github

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass
   public static void main(String[] args) {
     System.out.println("test git");
     //przykladowa zmiana - test pull-a
+    //druga zmiana wprowadzona przez github
   }
 }
