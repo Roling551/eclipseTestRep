ab0819025b507f3cdd0befbb1c85a940812ad6bd
ErichLeister <jasonvorhe@interia.pl>
2016-09-22 20:17:16 +0200

    drugi komentarz od jana

--- src/TestPackage/TestClass.java
+++ src/TestPackage/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass
 {
   public static void main(String[] args) {
     System.out.println("test git");
-    System.out.println("komentarz od jana");
+    System.out.println("komentarz od jana i drugi komentarz");
+    
     //przykladowa zmiana - test pull-a
     //druga zmiana wprowadzona przez github
     //test push to upstream
